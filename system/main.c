0143e75c9814715469260a3224816c57d15a1a21
deolson <deolson@pugetsound.edu>
2017-04-03 15:06:07 -0700

    fixed quantum

--- system/main.c
+++ system/main.c
@@ -6,17 +6,15 @@
 void    printchar(char c)
 {
     int i;
-    for (i=0; i<30; i++)
-        kprintf("%c\n", c);
+    for (i=0; i<10; i++)
+        kprintf("%c", c);
 }
 
 int main(uint32 argc, uint32 *argv)
 {
     //priority of process is input as the 3rd argument of create()
     ready(create((void*) printchar, INITSTK, 15, "P1", 1, 'A'), FALSE);
-    ready(create((void*) printchar, INITSTK, 20, "P2", 1, 'B'), FALSE);
-    ready(create((void*) printchar, INITSTK, 10, "P3", 1, 'C'), FALSE);
-    ready(create((void*) printchar, INITSTK, 5, "P4", 1, 'D'), FALSE);
+    ready(create((void*) printchar, INITSTK, 15, "P2", 1, 'B'), FALSE);
 
     return 0;
 }
